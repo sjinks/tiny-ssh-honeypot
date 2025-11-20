8b08ecf59a8278d69963408fec6854b536b090b8
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-11-20 10:38:34 +0200

    feat: terse logs

--- src/log.c
+++ src/log.c
@@ -4,18 +4,24 @@
 #include <unistd.h>
 #include "log.h"
 
+int terse_logs = 0;
+
 void my_log(const char *format, ...)
 {
-    time_t now;
-    struct tm timeinfo;
-    char timestring[32];
+    if (terse_logs == 0) {
+        time_t now;
+        struct tm timeinfo;
+        char timestring[32];
 
-    time(&now);
-    localtime_r(&now, &timeinfo);
-    strftime(timestring, sizeof(timestring), "%F %T", &timeinfo);
+        time(&now);
+        localtime_r(&now, &timeinfo);
+        strftime(timestring, sizeof(timestring), "%F %T", &timeinfo);
 
-    fputs(timestring, stderr);
-    fputs(" tiny-ssh-honeypot: ", stderr);
+        fputs(timestring, stderr);
+        fputc(' ', stderr);
+    }
+
+    fputs("tiny-ssh-honeypot: ", stderr);
 
     va_list ap;
     va_start(ap, format);
